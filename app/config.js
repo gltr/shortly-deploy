956da3cff9131dd96c662e5b171afffac2cb4717
Hack Reactor Students <accounts@hackreactor.com>
2016-03-31 12:26:11 -0700

    swap out knex schema for mongo/mongoose schema (in process)

--- app/config.js
+++ app/config.js
@@ -1,39 +1,126 @@
 var path = require('path');
-var knex = require('knex')({
-  client: 'sqlite3',
-  connection: {
-    filename: path.join(__dirname, '../db/shortly.sqlite')
-  }
-});
-var db = require('bookshelf')(knex);
+var crypto = require('crypto');
+var bcrypt = require('bcrypt-nodejs');
 
-db.knex.schema.hasTable('urls').then(function(exists) {
-  if (!exists) {
-    db.knex.schema.createTable('urls', function (link) {
-      link.increments('id').primary();
-      link.string('url', 255);
-      link.string('baseUrl', 255);
-      link.string('code', 100);
-      link.string('title', 255);
-      link.integer('visits');
-      link.timestamps();
-    }).then(function (table) {
-      console.log('Created Table', table);
-    });
-  }
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+mongoose.connect('mongodb://localhost/mongotest');
+
+var db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'connection error: '));
+db.once('open', function() {
+  var urlSchema = mongoose.Schema({
+    id: Number,
+    url: String,
+    baseUrl: String,
+    code: String,
+    title: String,
+    visits: Number
+  });
+
+  var userSchema = mongoose.Schema({
+    id: Number,
+    username: String,
+    password: String
+  });
+
 });
 
-db.knex.schema.hasTable('users').then(function(exists) {
-  if (!exists) {
-    db.knex.schema.createTable('users', function (user) {
-      user.increments('id').primary();
-      user.string('username', 100).unique();
-      user.string('password', 100);
-      user.timestamps();
-    }).then(function (table) {
-      console.log('Created Table', table);
+urlSchema.methods.init = function(model, attrs, options) {
+  var shasum = crypto.createHash('sha1');
+  shasum.update(model.get('url'));
+  model.set('code', shasum.digest('hex').slice(0, 5));
+};
+
+
+userSchema.methods.init = function() {
+  this.hashPassword;
+};
+
+urlSchema.methods.comparePassword = function(attemptedPassword, callback) {
+  bcrypt.compare(attemptedPassword, this.get('password'), function(err, isMatch) {
+    callback(isMatch);
+  });
+};
+
+urlSchema.methods.hashPassword = function() {
+  var cipher = Promise.promisify(bcrypt.hash);
+  return cipher(this.get('password'), null, null).bind(this)
+    .then(function(hash) {
+      this.set('password', hash);
     });
-  }
-});
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 module.exports = db;
+
+
+
+//create a connection to mongodb instance
+
+
+
+
+
+
+
+
+
+//----------OLD SQLITE3+KNEX+BOOKSHELF CODE----------
+
+
+// var knex = require('knex')({
+//   client: 'sqlite3',
+//   connection: {
+//     filename: path.join(__dirname, '../db/shortly.sqlite')
+//   }
+// });
+// var db = require('bookshelf')(knex);
+
+// db.knex.schema.hasTable('urls').then(function(exists) {
+//   if (!exists) {
+//     db.knex.schema.createTable('urls', function (link) {
+//       link.increments('id').primary();
+//       link.string('url', 255);
+//       link.string('baseUrl', 255);
+//       link.string('code', 100);
+//       link.string('title', 255);
+//       link.integer('visits');
+//       link.timestamps();
+//     }).then(function (table) {
+//       console.log('Created Table', table);
+//     });
+//   }
+// });
+
+
+
+// db.knex.schema.hasTable('users').then(function(exists) {
+//   if (!exists) {
+//     db.knex.schema.createTable('users', function (user) {
+//       user.increments('id').primary();
+//       user.string('username', 100).unique();
+//       user.string('password', 100);
+//       user.timestamps();
+//     }).then(function (table) {
+//       console.log('Created Table', table);
+//     });
+//   }
+// });
+
+
+
--- app/models/link.js
+++ app/models/link.js
@@ -7,13 +7,13 @@ var Link = db.Model.extend({
   defaults: {
     visits: 0
   },
-  initialize: function() {
-    this.on('creating', function(model, attrs, options) {
-      var shasum = crypto.createHash('sha1');
-      shasum.update(model.get('url'));
-      model.set('code', shasum.digest('hex').slice(0, 5));
-    });
-  }
+  // initialize: function() {
+  //   this.on('creating', function(model, attrs, options) {
+  //     var shasum = crypto.createHash('sha1');
+  //     shasum.update(model.get('url'));
+  //     model.set('code', shasum.digest('hex').slice(0, 5));
+  //   });
+  // }
 });
 
 module.exports = Link;
